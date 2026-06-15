d8bd355936cb01b635c4fc56c8d1f639a7481973
Sonu-TitanEd <SonuK@TitanEd.com>
2026-06-15 22:11:55 +0530

    change scrollbar color

--- src/index.jsx
+++ src/index.jsx
@@ -12,6 +12,9 @@ import messages from './i18n';
 
 import './index.scss';
 
+document.documentElement.classList.add('custom-page-scroll');
+document.body.classList.add('custom-page-scroll');
+
 const container = document.getElementById('root');
 
 const root = createRoot(container);
