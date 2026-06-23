818802776a0da0e5c0a618c2028c5d51afb50fcb
Mahendra <MahendraC@TitanEd.com>
2026-06-23 13:41:38 +0530

    Added FAQ Page

---
+++ src/pages/FAQ/faq.message.jsx
@@ -0,0 +1,38 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+
+  // Page headings
+  'faq.breadcrumb.home': {
+    id: 'faq.breadcrumb.home',
+    defaultMessage: 'Home',
+    description: 'Breadcrumb link to faq',
+  },
+
+  'faq.breadcrumb.faq': {
+    id: 'faq.breadcrumb.faq',
+    defaultMessage: 'FAQ',
+    description: 'Breadcrumb link to faq',
+  },
+
+  'faq.title': {
+    id: 'faq.title',
+    defaultMessage: 'Frequently Asked Questions',
+    description: 'Main heading of the FAQ page',
+  },
+
+  'faq.subtitle': {
+    id: 'faq.subtitle',
+    defaultMessage: 'A VigyanShaala Program — She for STEM',
+    description: 'Subheading on FAQ page',
+  },
+
+  'faq.intro': {
+    id: 'faq.intro',
+    defaultMessage: "Find answers to the most common questions about the She for STEM Incubator, including program structure, eligibility, learning experience, certification, and career opportunities. If you don't find the information you're looking for, please contact our program team.",
+    description: 'Intro paragraph on FAQ page',
+  },
+
+});
+
+export default messages;
