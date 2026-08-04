5ddd3d7f5ccc0143434f94a2acec23727173f0fc
Mahendra <MahendraC@TitanEd.com>
2026-08-04 18:56:48 +0530

    Updated images

--- src/components/HeroCarousel/HeroCarousel.jsx
+++ src/components/HeroCarousel/HeroCarousel.jsx
@@ -5,9 +5,9 @@ import { Button } from '@openedx/paragon';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
-import hero_1 from '../../assets/image/Home-images/hero_1.jpg';
-import hero_2 from '../../assets/image/Home-images/hero_2.jpg';
-import hero_3 from '../../assets/image/Home-images/hero_3.jpg';
+import hero_1 from '../../assets/image/Home-images/hero_1.png';
+import hero_2 from '../../assets/image/Home-images/hero_2.png';
+import hero_3 from '../../assets/image/Home-images/hero_3.png';
 
 import messages from '../../message/GlobalMessage.message';
 import './HeroCarousel.scss';
